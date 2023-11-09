e6114bcf88d45150ce4eabf8bc6a67985a80b171
joão vitor <105920560+007hfiueyw79f279pruip@users.noreply.github.com>
2023-11-09 08:41:24 -0300

    1

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-alert("oi rs")
+function tocaSom (idElementoAudio){
+    document.querySelectorAll(idElementoAudio).play();
+    }
+    const listaDeTeclas = document.querySelectorAll("tecla")
+    
+    let contador = 0;
+    while(contador<listaDeTeclas.length){
+        const efeito = listaDeTeclas[contador].classList[1];
+        const idAudio = "#som_"+efeito;
+        listaDeTeclas[contador].onlick = function(){
+            tocaSom(idAudio)
+        }
+        contador = contador + 1
+        console.log(contador);
+    }
