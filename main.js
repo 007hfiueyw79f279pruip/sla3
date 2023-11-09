e2a86f6bcfadeb9fb23117dc3bac363e738bf4f9
joão vitor <105920560+007hfiueyw79f279pruip@users.noreply.github.com>
2023-11-09 08:46:57 -0300

    5

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function tocaSom (idElementoAudio){
     document.querySelectorAll(idElementoAudio).play();
     }
-    const listaDeTeclas = document.querySelectorAll("tecla")
+    const listaDeTeclas = document.querySelectorAll(".tecla")
     
     let contador = 0;
     while(contador<listaDeTeclas.length){
